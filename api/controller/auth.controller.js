f9d15afa3ece93ad4d1a35730ea389168be439c8
Abhishek <abhisheksonawane1997@gmail.com>
2024-04-11 09:53:50 +0530

    add OAuth functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -58,3 +58,40 @@ export const signin = async(req, res, next) => {
     }
 };
 
+export const google = async(req, res, next) => {
+
+    const { email, name, googlePhotoUrl } = req.body;
+
+    if(!email || email === ''){
+        next(errorHandler( 400,'All Fields Are Required' ));
+    }
+    else{
+        try{
+            const validUser = await User.findOne({ email });
+            if(!validUser) next(errorHandler(404, 'User Not Found'));
+            if(validUser){
+                const token = jwt.sign({ id:validUser._id }, process.env.JWT_SECRETE );
+                const { password , ...rest } = validUser._doc;
+                res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest);
+            }
+            else{
+                const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+                const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+                const newUser =  new User({
+                    username : name.toLowerCase().split(" ").join("") + Math.random().toString(9).slice(-4),
+                    email,
+                    password : hashedPassword,
+                    profilePicture : googlePhotoUrl,
+                });
+                await newUser.save();
+                const token = jwt.sign({ id:newUser._id }, process.env.JWT_SECRETE );
+                const { password , ...rest } = newUser._doc;
+                res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest);
+            }
+        }catch(err){
+            next(err)
+        }
+    }
+};
+
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice';
+import OAuth from '../components/OAuth';
 
 const SignIn = () => {
 
@@ -84,6 +85,7 @@ const SignIn = () => {
                 ) : 'Sign In' 
               }
             </Button>
+            <OAuth/>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Don't Have and account ?</span>
