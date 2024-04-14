33638f4477d60494913dbb4af3d7bd152588731b
Abhishek <abhisheksonawane1997@gmail.com>
2024-04-14 17:19:59 +0530

    add create post page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import SignIn from './pages/SignIn'
 import Header from './components/Header'
 import FooterComp from './components/FooterComp'
 import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,6 +28,9 @@ function App() {
         <Route path='/sign-up' element={ <SignUp /> } />
         <Route element={ <PrivateRoute /> } >
         <Route path='/dashboard' element={ <Dashboard /> } />
+        </Route>
+        <Route element={ <OnlyAdminPrivateRoute /> } >
+        <Route path='/create-post' element={ <CreatePost /> } />
         </Route>
         <Route path='/projects' element={ <Projects /> } />
       </Routes>
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -2,7 +2,7 @@ import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import React, { useState, useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { HiOutlineExclamationCircle } from "react-icons/hi";
-
+import { Link } from 'react-router-dom';
 import {
   getDownloadURL,
   getStorage,
@@ -23,7 +23,7 @@ import {
 } from "../redux/user/userSlice";
 
 const DashProfile = () => {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error, loading } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const filePickerRef = useRef();
@@ -226,9 +226,14 @@ const DashProfile = () => {
           placeholder="password"
           onChange={handleChange}
         />
-        <Button type="submit" gradientDuoTone={"purpleToBlue"} outline>
-          Update
+        <Button type="submit" gradientDuoTone={"purpleToBlue"} outline disabled={ loading || imageFileUploading }>
+          {loading? "Loading..." : 'Update'}
         </Button>
+        {
+          currentUser.isAdmin && <Link to={"/create-post"}><Button type="button" gradientDuoTone={"purpleToPink"} className="w-full">
+          Create a post
+        </Button></Link>
+        }
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span className="cursor-pointer" onClick={() => setShowModal(true)}>
