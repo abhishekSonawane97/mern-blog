831044cc6e5ce2c05d3eeebf08d65862ac00104a
Abhishek <abhisheksonawane1997@gmail.com>
2024-04-16 08:43:08 +0530

    add posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Sidebar } from 'flowbite-react';
-import { HiArrowSmRight, HiUser } from 'react-icons/hi';
+import { HiArrowSmRight, HiDocumentText, HiUser } from 'react-icons/hi';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 
@@ -12,6 +12,7 @@ const DashSidebar = () => {
     const location = useLocation();
     const [tab, setTab ] = useState("");
     const dispatch = useDispatch();
+    const { currentUser } = useSelector(state => state.user)
 
     useEffect(()=>{
 
@@ -44,12 +45,20 @@ const DashSidebar = () => {
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
-            <Sidebar.ItemGroup>
+            <Sidebar.ItemGroup className='flex flex-col gap-1'>
                 <Link to={'/dashboard?tab=profile'}>
-                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={"User"} labelColor="dark" as='div'>
+                <Sidebar.Item active={tab==='profile'} icon={HiUser} label={ currentUser.isAdmin ? "Admin" : "User" } labelColor="dark" as='div'>
                     profile
                 </Sidebar.Item>
                 </Link>
+                {
+                  currentUser.isAdmin && <Link to={'/dashboard?tab=posts'} >
+                  <Sidebar.Item active={tab==='posts'} icon={ HiDocumentText } as='div'>
+                    Posts
+                  </Sidebar.Item>
+                </Link>
+                }
+                
                 <Link to={'/sign-in'} onClick={handleSignout}>
                 <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer" as='div'>
                     Sign Out
