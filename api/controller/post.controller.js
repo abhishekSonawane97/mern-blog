cc57f12176fd83645ab7c7c9e43fd453de5aa6e5
Abhishek <abhisheksonawane1997@gmail.com>
2024-04-22 09:41:40 +0530

    update post functionality

--- api/controller/post.controller.js
+++ api/controller/post.controller.js
@@ -81,3 +81,23 @@ export const deletePost = async(req, res, next ) =>{
         next(error);
     }
 };
+
+export const updatePost = async(req, res, next ) =>{
+    if(!req.user.isAdmin || req.user.id !== req.params.userId){
+        return next(403, 'You are not allowed to update this post.')
+    }
+    try{
+        const updatedPost = await Post.findByIdAndUpdate(req.params.postId, {
+            $set : {
+                title : req.body.title,
+                content : req.body.content,
+                category : req.body.category,
+                image : req.body.image,
+            }
+        }, { new : true });
+    res.status(200).json('The post has been updated : ', updatedPost);
+    }
+    catch(error){
+        next(error);
+    }
+};
