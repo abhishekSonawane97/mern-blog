201c1f2bb7ea02bed09409f7c33e2fb458393717
Abhishek <abhisheksonawane1997@gmail.com>
2024-04-27 07:43:51 +0530

    add create comment api

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoutes from './routes/user.route.js';
 import authRoute from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/post.route.js';
 
 
 dotenv.config();
@@ -28,6 +29,7 @@ app.listen(3000, () => {
  app.use("/api/user", userRoutes);
  app.use("/api/auth", authRoute );
  app.use("/api/post", postRoutes );
+ app.use("/api/comment", commentRoutes );
 
  app.use(( err, req, res, next ) => {
     const statusCode = err.statusCode || 500;
