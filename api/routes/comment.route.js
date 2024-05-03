3cf22ab9e012e1bd6694b59ced72f3a6c2e22dba
Abhishek <abhisheksonawane1997@gmail.com>
2024-05-03 22:28:43 +0530

    add dashboard overview to the admin dashboard

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { createComment, getPostComment, likeComment, editComment, deleteComment } from '../controller/comment.controller.js'
+import { createComment, getPostComment, likeComment, editComment, deleteComment, getComments } from '../controller/comment.controller.js'
 
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.get('/getPostComments/:postId', getPostComment);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
 router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+router.get('/getComments', verifyToken,  getComments);
 
 export default router;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,6 +4,8 @@ import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
+import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
 const Dashboard = () => {
 
@@ -35,6 +37,12 @@ const Dashboard = () => {
       {/* users   */}
       {
         tab === 'users' && <DashUsers />
+      }
+      {
+        tab === 'comments' && <DashComments />
+      }
+      {
+        tab === 'dash' && <DashboardComp />
       }
     </div>
   )
