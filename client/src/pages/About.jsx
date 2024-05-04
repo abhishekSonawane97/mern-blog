fe40d22bcc1e1119bc46867aa0e1ec6ecaf37a09
Abhishek <abhisheksonawane1997@gmail.com>
2024-05-04 19:39:31 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <div className='min-h-screen flex items-center justify-center'>
       
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div className="">
+          <h1 className="text-3xl font font-semibold text-center my-7">About Tech Blogs</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p className="">
+              Welcome to the Tech Blogs! This blog was created by abhishek sonawane as a personal project to share his thoughts and ideas with the world. abhishek is a passionate developer who loves to write about technology, coding and everything in between.
+            </p>
+            <p>
+              On this blog, you will find articles and tutorials on topics such as web development, software engineering, and programming languages. abhishek is awlays learing and exploring new technologies, so be sure to check back often for new content!
+            </p>
+            <p className="">
+              WWe encourage you to leave comments on our posts and engage with other readers. You can like other people's comments and reply to them as well. We believe that a community of learners can help each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
